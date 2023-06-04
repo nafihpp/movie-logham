dd771794a9c05c48d247c14862d710f4a10dece4
nafihpp <nafihpp@gmail.com>
2023-06-04 16:27:50 +0530

    bug-fix

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,13 +1,21 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    //true === dark
     const [theme, setTheme] = useState("dark");
     const toggleTheme = () => {
         setTheme(theme === "dark" ? "light" : "light" && "dark");
     };
+    useEffect(() => {
+        if (theme === "light") {
+            let doc = document.getElementsByTagName("BODY")[0];
+            doc.style.background = "#fff";
+        } else {
+            let doc = document.getElementsByTagName("BODY")[0];
+            doc.style.background = "#000";
+        }
+    }, [theme]);
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
